1041b104b97b3d21d1f93595a12c0775600c7664
Boris Borisov <bobbyborisov16@gmail.com>
2023-06-13 10:30:29 +0300

    topological sort update

--- introduction to graphs/topological_sort.py
+++ introduction to graphs/topological_sort.py
@@ -1,14 +1,3 @@
-nodes = int(input())
-
-graph = {}
-
-for _ in range(nodes):
-    line_parts = input().split('->')
-    node = line_parts[0].strip()
-    children = line_parts[1].strip().split(', ') if line_parts[1] else []
-    graph[node] = children
-
-
 def find_dependencies(graph):
     result = {}
     for node, children in graph.items():
@@ -23,5 +12,39 @@ def find_dependencies(graph):
     return result
 
 
+def find_node_without_dependency(dep):
+    for node, dependencies in dep.items():
+        if dependencies == 0:
+            return node
+    return None
+
+
+nodes = int(input())
+graph = {}
+
+for _ in range(nodes):
+    line_parts = input().split('->')
+    node = line_parts[0].strip()
+    children = line_parts[1].strip().split(', ') if line_parts[1] else []
+    graph[node] = children
+
 dep = find_dependencies(graph)
-print(dep)
+
+has_cycles = False
+sorted_nodes = []
+
+while dep:
+    node_to_remove = find_node_without_dependency(dep)
+    if node_to_remove is None:
+        has_cycles = True
+        break
+    dep.pop(node_to_remove)
+    sorted_nodes.append(node_to_remove)
+
+    for child in graph[node_to_remove]:
+        dep[child] -= 1
+
+if has_cycles:
+    print('Invalid topological sorting')
+else:
+    print(f"Topological sorting: {', '.join(sorted_nodes)}")
