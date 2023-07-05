f222121411b223c56343e2a1e2586d3d9783f82c
Boris Borisov <bobbyborisov16@gmail.com>
2023-07-05 10:47:52 +0300

    dynamic programming exercises

--- dynamic programming/lis.py
+++ dynamic programming/lis.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 nums = [int(x) for x in input().split()]
 
 size = [0] * len(nums)
@@ -7,6 +8,7 @@ size[0] = 1
 parent = [None] * len(nums)
 best_idx = 0
 best_size = 1
+
 for current in range(1, len(nums)):
     current_number = nums[current]
     current_best_size = 1
@@ -36,4 +38,4 @@ while best_idx is not None:
     result.appendleft((nums[best_idx]))
     best_idx = parent[best_idx]
 
-print(*result,sep = ' ')
+print(*result, sep=' ')
