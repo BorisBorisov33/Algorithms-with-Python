e2ee7bec72ebd4438865224da92c97c3b006687d
Boris Borisov <bobbyborisov16@gmail.com>
2023-07-11 16:28:57 +0300

    update of exercises

--- Recursion/labirint.py
+++ Recursion/labirint.py
@@ -13,11 +13,11 @@ def find_all_paths(row, col, lab, direction, path):
         print(''.join(path))
     else:
         lab[row][col] = 'v'
-
-        find_all_paths(row - 1, col, lab, 'U', path)
-        find_all_paths(row + 1, col, lab, 'D', path)
-        find_all_paths(row, col - 1, lab, 'L', path)
         find_all_paths(row, col + 1, lab, 'R', path)
+        find_all_paths(row, col - 1, lab, 'L', path)
+        find_all_paths(row + 1, col, lab, 'D', path)
+        find_all_paths(row - 1, col, lab, 'U', path)
+
         lab[row][col] = '-'
     path.pop()
 
@@ -28,4 +28,4 @@ lab_array = []
 for _ in range(rows):
     lab_array.append(list(input()))
 
-    find_all_paths(0, 0, lab_array, '', [])
+find_all_paths(0, 0, lab_array, '', [])
