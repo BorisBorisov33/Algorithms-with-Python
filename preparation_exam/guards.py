cdffd0389d4e0f70b8eb99bb929b3aede425b5c4
Boris Borisov <bobbyborisov16@gmail.com>
2023-07-18 16:49:44 +0300

    update of an exam task

--- preparation_exam/guards.py
+++ preparation_exam/guards.py
@@ -1,33 +1,36 @@
-def find_dependencies(graph):
-    result = {}
-    for node, children in graph.items():
-        if node not in result:
-            result[node] = 0
-
-        for child in children:
-            if child not in result:
-                result[child] = 1
+from collections import deque
+# BFS approach of the task
+def countNotReachusingBFS(gr, n):
+    visited = [False] * (n + 1)
+    pq = deque()
+    pq.append(start_node)
+    while pq:
+        start = pq.popleft()
+        visited[start] = True
+        for iterable_value in gr[start]:
+            if visited[iterable_value]:
+                continue
             else:
-                result[child] += 1
-    return result
+                pq.append(iterable_value)
 
+    result_array = []
+    for i in range(n + 1):
+        if i == start_node:
+            visited[i] = True
+        if i == 0:
+            continue
 
-def find_node_without_dependencies(dep):
-    for node, dependencies in dep.items():
-        if dependencies == 0:
-            return node
-    return None
+        if not visited[i]:
+            result_array.append(i)
+    print(*result_array)
 
-
-nodes = int(input())
-edges = int(input())
 
 graph = {}
-
+nodes = int(input())
+edges = int(input())
 for _ in range(edges):
-    source, destination = input().split()
-    source = int(source)
-    destination = int(destination)
+
+    source, destination = [int(x) for x in input().split()]
 
     if source not in graph:
         graph[source] = []
@@ -37,26 +40,4 @@ for _ in range(edges):
 
 start_node = int(input())
 
-has_cycles = False
-sorted_nodes = []
-
-dep = find_dependencies(graph)
-print(dep)
-
-
-
-while dep:
-    node_to_remove = find_node_without_dependencies(dep)
-    # print(node_to_remove)
-
-    if node_to_remove is None:
-        has_cycles = True
-        break
-
-    sorted_nodes.append(node_to_remove)
-    dep.pop(node_to_remove)
-
-    for child in graph[node_to_remove]:
-        dep[child] -= 1
-
-print(sorted_nodes)
+countNotReachusingBFS(graph, nodes)
